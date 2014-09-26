056beeb179e911246b2a37de37a81725fdb677a7
Идель Гизатуллин <gizatullin@pr.tt>
2014-09-26 22:52:24 +0600

    dynamic routing
    just see /blog/2

--- web/index.php
+++ web/index.php
@@ -9,4 +9,40 @@ $app->get('/hello', function() {
     return 'Hello!';
 });
 
+$blogPosts = array(
+    1 => array(
+        'date'      => '2011-03-29',
+        'author'    => 'igorw',
+        'title'     => 'Using Silex',
+        'body'      => 'Visiting /blog will return a list of blog post titles. The use statement means something different in this context. It tells the closure to import the $blogPosts variable from the outer scope. This allows you to use it from within the closure.',
+    ),
+    2 => array(
+        'date'      => '2011-03-29',
+        'author'    => 'edele',
+        'title'     => 'But does it float?',
+        'body'      => '<img src="http://payload275.cargocollective.com/1/0/128/7818950/10_Muzika03_905.jpg">',
+    ),
+);
+
+$app->get('/blog', function () use ($blogPosts) {
+    $output = '';
+    foreach ($blogPosts as $post) {
+        $output .= $post['title'];
+        $output .= '<br />';
+    }
+
+    return $output;
+});
+
+$app->get('/blog/{id}', function (Silex\Application $app, $id) use ($blogPosts) {
+    if (!isset($blogPosts[$id])) {
+        $app->abort(404, "Post $id does not exist.");
+    }
+
+    $post = $blogPosts[$id];
+
+    return  "<h1>{$post['title']}</h1>".
+            "<p>{$post['body']}</p>";
+});
+
 $app->run();
