c8967c08d4bf67471979e260309c705227a3c122
Идель Гизатуллин <gizatullin@pr.tt>
2014-09-27 09:23:15 +0600

    Now I know how to use components. Tried out on Symphony's Filesystem component

--- web/index.php
+++ web/index.php
@@ -1,12 +1,12 @@
 <?php
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Filesystem;
 
 require_once __DIR__.'/../vendor/autoload.php';
 
 $app = new Silex\Application();
 $app['debug'] = true;
-
 
 
 $app->get('/', function() {
@@ -61,6 +61,12 @@ $app->post('/feedback', function (Request $request) {
     return new Response('You sent: <i>'.$message.'</i><br>Thank you for your feedback!', 201);
 });
 
+$app->error(function (\LogicException $e, $code) {
+    // this handler will only handle \LogicException exceptions
+    // and exceptions that extends \LogicException
+    return new Response("Logic Error!");
+});
+
 
 $app->error(function (\Exception $e, $code) {
     switch ($code) {
@@ -71,6 +77,26 @@ $app->error(function (\Exception $e, $code) {
             $message = 'We are sorry, but something went terribly wrong.';
     }
     return new Response($message);
+});
+
+
+$app->get('/copy/{originFile}/{targetFile}', function (Silex\Application $app, Filesystem $fs, $originFile, $targetFile) {
+    /*if (!isset($blogPosts[$id])) {
+        $app->abort(404, "Post $id does not exist.");
+    }*/
+
+    try {
+    	$fs->copy($originFile, $targetFile);
+    } catch (Exception $e) {
+    	print_r($e);
+    	$app->abort(404, "Error copying file");
+    }
+
+    return  "Copied $originFile to $targetFile!";
+});
+
+$app->get('/copy', function () {
+    return  "You can copy files";
 });
 
 $app->run();
